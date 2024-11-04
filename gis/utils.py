12d0f495d0511facbed97d787be6d20e027060a6
Eric Dasmalchi <edasmalchi@gmail.com>
2024-11-03 19:56:44 -0800

    gis work

---
+++ gis/utils.py
@@ -0,0 +1,7 @@
+WGS84 = "EPSG:4326"
+CA_StatePlane = "EPSG:2229"  # units are in feet
+CA_NAD83Albers = "EPSG:3310"  # units are in meters
+
+SQ_MI_PER_SQ_M = 3.86 * 10**-7
+FEET_PER_MI = 5_280
+SQ_FT_PER_SQ_MI = 2.788 * 10**7
